995e42fd9ff97b8861c2117256181cc1049a462b
matthias <schmid_mat@gmx.at>
2011-05-28 16:16:55 +0200

    added install_components.sh and install_sensors.sh

---
+++ install_sensors.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# @author: Matthias Schmid
+# @date: 27.05.2011
+#
+# installs the sensors and start_sensor.sh to /etc/init.d/ for immediate autostart
+
+# Installation Path
+install_path=/usr/share/roadrunner
+
+# Install File Definition
+simserver='temp_sensor.js'
+simulator='temperature.js'
+
+lib='lib'
+
+# Create Install Path
+sudo rm -rf $install_path
+sudo mkdir $install_path
+
+# Install Files into Install Path
+sudo cp $lib/$simserver $install_path/$simserver
+sudo cp $lib/$simulator $install_path/$simulator
+
+
+# autostart
+if [ "$1" = "--autostart" ]; then	
+	rm /etc/init.d/start_sensor.sh
+	chmod +x start_sensor.sh
+	cp start_sensor.sh /etc/init.d/start_sensor.sh
+fi
+
+
